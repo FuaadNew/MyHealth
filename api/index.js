fd888c6ce65e28bd08155e33dc42af7c1ba54c19
Fuaad <pawn@pawns-MacBook-Air.local>
2024-04-10 08:48:11 -0500

    test

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const express = require("express"),
 	LocalStrategy = require("passport-local"),
 	passportLocalMongoose = 
 		require("passport-local-mongoose")
-const User = require("./model/User");
+const User = require("../model/User");
 let app = express();
 
 mongoose.connect("mongodb://localhost/27017");
